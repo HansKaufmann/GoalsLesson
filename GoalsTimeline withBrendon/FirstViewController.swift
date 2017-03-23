9cbee2b99cae30c19ea00d490e41bf38e4c24640
Hans Kaufmann <goalsventure@gmail.com>
2017-03-23 14:34:37 +0100

    Initial Commit

---
+++ GoalsTimeline withBrendon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GoalsTimeline withBrendon
+//
+//  Created by hans kaufmann on 23.03.17.
+//  Copyright © 2017 hans kaufmann. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
